28043f0774b8a820096693902a648df04a61effd
M-a-x-G <gregor@fh-brandenburg.de>
2014-12-11 15:56:44 +0100

    searchbar  fixes

--- app/controllers/search.js
+++ app/controllers/search.js
@@ -7,7 +7,15 @@ export default Ember.ArrayController.extend({
   isSearched: function(){
     return this.get("value") !== "" && this.get("title") !== "";
   }.property("key", "value"),
-
+  searchButtonText: function () {
+    var key = this.get("key").toLowerCase();
+    if (key === "title" || key === "authors" || key === "publisher" || key === "year") {
+      return key;
+    } else {
+      this.set("key", "title");
+      return "title";
+    }
+  }.property("key"),
   actions: {
     select: function (id) {
       this.set("key", id);

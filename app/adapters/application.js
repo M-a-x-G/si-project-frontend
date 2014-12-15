f028b65df8202d76cc827f8bdeda54254f5f7902
M-a-x-G <gregor@fh-brandenburg.de>
2014-12-15 22:31:30 +0100

    endpoint changed to production

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
-  host: 'http://localhost:8080',
+  host: 'http://gregmuebookworm.elasticbeanstalk.com',
   namespace: 'rest/v1'
 
 });
